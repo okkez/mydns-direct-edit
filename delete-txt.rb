ea6b2bf6b839d5cbd841b9ceb71c199097b52cec
okkez <okkez000@gmail.com>
2019-11-16 22:47:00 +0900

    Initial commit

---
+++ delete-txt.rb
@@ -0,0 +1,38 @@
+require "net/http"
+require "uri"
+require "yaml"
+
+MYDNS_URL = "https://www.mydns.jp/directedit.html"
+
+CERTBOT_ENV_NAMES = %w[
+  CERTBOT_DOMAIN
+  CERTBOT_VALIDATION
+  CERTBOT_TOKEN
+  CERTBOT_CERT_PATH
+  CERTBOT_KEY_PATH
+  CERTBOT_SNI_DOMAIN
+  CERTBOT_AUTH_OUTPUT
+]
+
+domain = ENV["CERTBOT_DOMAIN"]
+
+configs = YAML.load_file(File.join(__dir__, "config.yml"))
+account = configs[domain]
+
+params = {}
+CERTBOT_ENV_NAMES.each do |key|
+  params[key] = ENV[key]
+end
+
+params["EDIT_CMD"] = "DELETE"
+
+uri = URI.parse(MYDNS_URL)
+
+Net::HTTP.start(uri.host, port: uri.port) do |http|
+  request = Net::HTTP::Post.new(uri.path)
+  request.basic_auth(account["master_id"], account["master_password"])
+  request.set_form_data(params)
+  response = http.request(request)
+  p response
+  pp response.body
+end
